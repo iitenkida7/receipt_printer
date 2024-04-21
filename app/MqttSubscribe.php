2e8ebcf59c3fac216d127a904d566d1f453b3cc3
iitenkida7 <iitenkida7@users.noreply.github.com>
2024-04-21 23:46:32 +0000

    Rename

--- app/MqttSubscribe.php
+++ app/MqttSubscribe.php
@@ -6,7 +6,7 @@ use  Dotenv\Dotenv;
 use \PhpMqtt\Client\MqttClient;
 use \PhpMqtt\Client\ConnectionSettings;
 
-class MqttSubScribe
+class MqttSubscribe
 {
   private string $topic = 'php/mqtt';
   private string $server;
