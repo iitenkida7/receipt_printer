f22d4724ea4387f8d8fb4cd8f31d9683857994df
iitenkida7 <iitenkida7@users.noreply.github.com>
2024-05-01 14:13:20 +0000

    Add logger

--- app/Mqtt.php
+++ app/Mqtt.php
@@ -5,9 +5,14 @@ namespace App;
 use  Dotenv\Dotenv;
 use \PhpMqtt\Client\MqttClient;
 use \PhpMqtt\Client\ConnectionSettings;
+use Monolog\Logger;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
 
 class Mqtt
 {
+  public bool $debug = false;
+
   private string $topic = 'php/mqtt';
   private string $server;
   private int $port;
@@ -39,7 +44,8 @@ class Mqtt
 
   public function subscribe(): void
   {
-    $mqtt = new MqttClient($this->server, $this->port, $this->clientId, MqttClient::MQTT_3_1_1);
+    $logger = $this->debug ? $this->logger() : null;
+    $mqtt = new MqttClient($this->server, $this->port, $this->clientId, MqttClient::MQTT_3_1_1, null, $logger);
     $mqtt->connect($this->connectionSettings, $this->clean_session);
     $mqtt->subscribe($this->topic, function ($topic, $message) {
       $this->ReceivedPostProcess($topic, $message);
@@ -51,9 +57,11 @@ class Mqtt
 
   public function publish(string $message): void
   {
-    $mqtt = new MqttClient($this->server, $this->port, $this->clientId, MqttClient::MQTT_3_1_1);
+
+    $logger = $this->debug ? $this->logger() : null;
+    $mqtt = new MqttClient($this->server, $this->port, $this->clientId, MqttClient::MQTT_3_1_1, null,  $logger);
     $mqtt->connect($this->connectionSettings, $this->clean_session);
-    $mqtt->publish($this->topic, $message, 0);
+    $mqtt->publish($this->topic, $message,  MqttClient::QOS_AT_MOST_ONCE);
     $mqtt->disconnect();
   }
 
@@ -61,5 +69,15 @@ class Mqtt
   {
     // override this method to process the received message
     printf("Received message on topic [%s]: %s\n", $topic, $message);
+  }
+
+  private function logger(): Logger
+  {
+    date_default_timezone_set("Asia/Tokyo");
+    $formatter = new LineFormatter("[%datetime%] %channel% %level_name% %message%\n", "Y-m-d H:i:s");
+    $logger = new Logger('log');
+    $logHandler = new StreamHandler('php://stdout');
+    $logHandler->setFormatter($formatter);
+    return $logger->pushHandler($logHandler);
   }
 }
